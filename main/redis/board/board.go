99d16c1005cf617a7cb5cfee56fd91b8e26366f3
Zhaijinyi <161276669+Zhaijinyi@users.noreply.github.com>
2024-04-17 09:27:54 +0800

    Update board.go

--- main/redis/board/board.go
+++ main/redis/board/board.go
@@ -7,12 +7,12 @@ import (
 // 向排行榜中添加成员及其分数
 func AddMemberAndScore(client *redis.Client, member string, score float64) error {
 	z := redis.Z{Score: score, Member: member}
-	err := client.ZAdd("board", z).Err() //
+	err := client.ZAdd("board", z).Err() //向有序集合中添加一个或多个成员
 	return err
 }
 
 // 获取排行榜前N名成员
 func GetTopMembers(client *redis.Client, n int) ([]string, error) {
-	members, err := client.ZRevRange("board", 0, int64(n-1)).Result()
+	members, err := client.ZRevRange("board", 0, int64(n-1)).Result()//从高到低排序 ZRange从低到高排序
 	return members, err
 }
